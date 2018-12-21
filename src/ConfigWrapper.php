60309d785a9ce3da47dd2fc5454a6ea5cbe53ac0
p_tedcliang <liangchen@yuewen.com>
2018-12-21 21:59:45 +0800

    add config file

---
+++ src/ConfigWrapper.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liangchen
+ * Date: 2018/12/20
+ * Time: 22:53
+ */
+
+namespace Tars\config;
+
+
+class ConfigWrapper
+{
+    public $_configServant;
+
+    public function __construct($tarsClientConf, $socketMode = 2)
+    {
+        $config = new \Tars\client\CommunicatorConfig();
+        $locator = $tarsClientConf['locator'];
+        $moduleName = $tarsClientConf['modulename'];
+
+        $config->setLocator($locator);
+        $config->setModuleName($moduleName);
+        $config->setSocketMode($socketMode);
+
+        $this->_configServant = new ConfigServant($config);
+    }
+
+}
